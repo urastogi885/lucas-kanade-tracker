bcb21afae47aee051bc73f0354139a6eeeccebc3
umang <umang.rastogi96@gmail.com>
2020-04-18 20:05:46 -0400

    [lk_tracker] Add comments for better documentation

--- Code/utils/lk_tracker.py
+++ Code/utils/lk_tracker.py
@@ -8,9 +8,12 @@ def extract_locations(dataset_location):
     :param dataset_location: location of the folder
     :return: a list containing proper location of all JPEG images
     """
+    # Define an empty list to store file locations
     filename_array = []
+    # Add file locations to the list
     for filename in glob(str(dataset_location) + '*.jpg'):
         filename_array.append(filename)
-
-    return filename_array.sort()
+    # Sort the list and return it
+    filename_array.sort()
+    return filename_array
 
