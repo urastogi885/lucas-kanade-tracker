2f56b9e56d7242c3f988a0572746ebb237abf620
umang <umang.rastogi96@gmail.com>
2020-04-20 15:30:44 -0400

    [main] Add total least square method using equalize histogram
    Get image shape from first image and feed it to the recorder
    
    Retreive roi points from text image

--- Code/main.py
+++ Code/main.py
@@ -1,39 +1,46 @@
 import cv2
-from numpy import zeros
 from sys import argv
+from numpy import zeros
 from utils import lk_tracker
 
 """
 :param dataset_location: Path of dataset for tracking relative to the project folder
 """
-script, dataset_location, output_location, select_roi = argv
+script, dataset, dataset_location, output_location, select_roi = argv
 
 
 if __name__ == '__main__':
-    video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-    video_output = cv2.VideoWriter(str(output_location), video_format, 10.0, (640, 360))
     # Get full image-locations from the given path
     img_locations = lk_tracker.extract_locations(dataset_location)
     template_image = cv2.imread(img_locations[0])
+    width, height, _ = template_image.shape
+    # Define parameters for recording output video
+    video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+    video_output = cv2.VideoWriter(str(output_location), video_format, 30.0, (height, width))
     template_image = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
+    # Add total least squares method for robustness
     template_image = cv2.equalizeHist(template_image) / 255
     warp_prev = zeros(2)
-    # TODO: Define best points here
-    roi_points = [155, 75, 60, 77]
+    # Retrieve saved roi points for best results
+    roi_points = lk_tracker.get_roi_points(dataset)
+    if roi_points is None:
+        select_roi = 1
     if int(select_roi):
         roi_points = list(cv2.selectROI('ROI', template_image))
+    prev_box = roi_points[0], roi_points[1]
     for img_location in img_locations:
         img_frame = cv2.imread(img_location)
         gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)
+        # Add total least squares method for robustness
         gray = cv2.equalizeHist(gray) / 255
         warp_prev = lk_tracker.affine_lk_tracker(gray, template_image, roi_points, warp_prev)
         bounding_box = roi_points[0] + int(warp_prev[0]), roi_points[1] + int(warp_prev[1])
+        if 0 > bounding_box[0] or bounding_box[0] >= width or 0 > bounding_box[1] or bounding_box[1] >= height:
+            bounding_box = prev_box
         cv2.rectangle(img_frame, bounding_box,
-                      (bounding_box[0] + roi_points[2], bounding_box[1] + roi_points[3]), (0, 255, 0), 3)
-        cv2.imshow("output", img_frame)
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
+                      (bounding_box[0] + roi_points[2], bounding_box[1] + roi_points[3]),
+                      (0, 255, 0), 3)
+        prev_box = bounding_box
         video_output.write(img_frame)
     video_output.release()
     cv2.destroyAllWindows()
